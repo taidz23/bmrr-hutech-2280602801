fc467413c34fde1e7184270282cc610d57f4bde7
taidz23 <nguyenhuuphuctai2k4@gmail.com>
2025-03-11 17:28:10 +0700

    Add files via upload

--- lab02/cipher/transposition/transposition_cipher.py
+++ lab02/cipher/transposition/transposition_cipher.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class TranspositionCipher:
     def __init__(self):
         pass
@@ -20,4 +21,28 @@ class TranspositionCipher:
             if col == key or (col == key - 1 and row >= len(text) % key):
                 col = 0
                 row += 1
+=======
+class TranspositionCipher:
+    def __init__(self):
+        pass
+
+    def encrypt(self, text, key):
+        encrypted_text = ''
+        for col in range(key):
+            pointer = col
+            while pointer < len(text):
+                encrypted_text += text[pointer]
+                pointer += key
+        return encrypted_text
+
+    def decrypt(self, text, key):
+        decrypted_text = [''] * key
+        row, col = 0, 0
+        for symbol in text:
+            decrypted_text[col] += symbol
+            col += 1
+            if col == key or (col == key - 1 and row >= len(text) % key):
+                col = 0
+                row += 1
+>>>>>>> 31c7f4ddc75f823439ad7ca8ef6c553f792c5ec7
         return ''.join(decrypted_text)
